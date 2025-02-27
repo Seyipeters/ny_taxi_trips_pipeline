74d650e930e5dd5f9611e503d9ab34461074bdd7
OMOLE PETER <49797093+Seyipeters@users.noreply.github.com>
2025-02-27 20:48:02 +0200

    Delete ny_taxi_trips_terraform directory

--- ny_taxi_trips_terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "The GCP project ID"
-  default     = "ny-taxi-trips-pipeline"
-}
-
-variable "region" {
-  description = "The region for the GCP resources"
-  default     = "us-central1"
-}
